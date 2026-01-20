0309d930a80bb0cec9316b591c892826ec75a1c5
Rahul dhaked <rdhaked440@gmail.com>
2026-01-20 21:35:09 +0530

    BUBBLE SORT PROBLEM

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 void bubbelsort(vector<int>& arr ) {
     int n= arr.size();
+    
     for(int i=0 ; i<n-1 ; i++){
+        bool isswap=false;
         for( int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 swap( arr[j] , arr[j+1]);
+                isswap=true;
             }
 
         }
-
+        if(!isswap){ // array is already sorted
+            break;
+        }
     }
 
 }
