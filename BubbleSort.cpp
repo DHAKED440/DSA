73ed6312d2ef450d8bf7008449d06393d83e24b9
Rahul dhaked <rdhaked440@gmail.com>
2026-01-20 21:26:06 +0530

    BUBBEL SORT PROBLEM

---
+++ BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void bubbelsort(vector<int>& arr ) {
+    int n= arr.size();
+    for(int i=0 ; i<n-1 ; i++){
+        for( int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap( arr[j] , arr[j+1]);
+            }
+
+        }
+
+    }
+
+}
+
+int main () {
+    vector<int>arr={4,1,5,2,3};
+    bubbelsort(arr);
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
