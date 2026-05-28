a62eb060cb6e7506e93253e8fdaa20c6b3726a3c
Rahul dhaked <rdhaked440@gmail.com>
2026-05-28 21:46:27 +0530

    common largest prifix

--- common prifix finder.cpp
+++ common prifix finder.cpp
@@ -7,12 +7,14 @@ int main (){
     vector<string>str={"dog","racecar","car"};
     string ans="";
 
- for(int i=0 ; i<str[0].size(); i++){
+ for(int i=0 ; i<str[0].size(); i++){    // we are check first string every element 
        char ch=str[0][i];
 
        bool match=true;
 
-      for(int j=0; j<str.size() ; j++){
+      for(int j=0; j<str.size() ; j++){  // by all string element if same index 
+        // char are not match or str size is less then  first string size then break
+
         if(str[j][i] != ch || i >= str[j].size() ){
             match=false;
             break;
